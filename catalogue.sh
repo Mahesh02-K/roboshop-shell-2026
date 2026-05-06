5f3af6c806c057decb2d9b1d038a766a9bc064e7
Mahesh02-K <kakuturumahesh@gmail.com>
2026-05-07 02:15:36 +0530

    roboshop-shell

--- catalogue.sh
+++ catalogue.sh
@@ -75,7 +75,7 @@ systemctl enable catalogue &>>$LOG_FILE
 systemctl start catalogue &>>$LOG_FILE
 VERIFY $? "Starting Catalogue"
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp $PWD/mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh - &>>$LOG_FILE
 VERIFY $? "Installing Mongodb client"
 
