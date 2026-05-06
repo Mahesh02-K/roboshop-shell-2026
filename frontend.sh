5340b6d016e143396f726204db81488236fe9b39
Mahesh02-K <kakuturumahesh@gmail.com>
2026-05-07 02:21:17 +0530

    roboshop-shell

--- frontend.sh
+++ frontend.sh
@@ -23,11 +23,21 @@ else
     exit 1
 fi
 
-VERIFY(){
+# VERIFY(){
+#     if [ $1 -eq 0 ]
+#     then 
+#         echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+#     else 
+#         echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+#         exit 1
+#     fi
+# }
+
+VERIFY(){ 
     if [ $1 -eq 0 ]
-    then 
+    then
         echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
-    else 
+    else
         echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
     fi
