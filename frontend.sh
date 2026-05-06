63038ee4fe60bdcead9942cb6ab29ed958db9952
Mahesh02-K <kakuturumahesh@gmail.com>
2026-05-07 02:32:10 +0530

    roboshop-shell

--- frontend.sh
+++ frontend.sh
@@ -52,12 +52,12 @@ VERIFY "Removing default content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip 
 VERIFY $? "Downloading frontend"
 
-rm -rf /etc/nginx/nginx.conf &>>$LOG_FILE
-VERIFY $? "Remove default nginx conf"
-
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip &>>$LOG_FILE
 VERIFY $? "Unzipping frontend into temp directory"
+
+rm -retc/nginx/nginx.conf &>>$LOG_FILE
+VERf /IFY $? "Remove default nginx conf"
 
 cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf &>>$LOG_FILE
 VERIFY $? "Copying nginx configuration file"
