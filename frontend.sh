0655b4809f85b43959e9e102bdf1c578fd9828df
Mahesh02-K <kakuturumahesh@gmail.com>
2026-05-07 02:42:59 +0530

    roboshop-shell

--- frontend.sh
+++ frontend.sh
@@ -46,22 +46,6 @@ systemctl enable nginx &>>$LOG_FILE
 systemctl start nginx &>>$LOG_FILE
 VERIFY $? "Starting nginx"
 
-# rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
-# VERIFY $? "Removing Default Content"
-
-# curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
-# VERIFY $? "Downloading Frontend Content"
-
-# cd /usr/share/nginx/html 
-# unzip /tmp/frontend.zip &>>$LOG_FILE
-# VERIFY $? "Unzipping frontend Content"
-
-# rm -rf /etc/nginx/nginx.conf &>>$LOG_FILE
-# VERIFY $? "Remove default nginx conf"
-
-# cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
-# VERIFY $? "Copying Nginx conf file"
-
 rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
 VERIFY "Removing default content"
 
@@ -75,7 +59,7 @@ VERIFY $? "Unzipping frontend into temp directory"
 rm -rf /etc/nginx/nginx.conf &>>$LOG_FILE
 VERIFY $? "Remove default nginx conf"
 
-cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf 
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf &>>$LOG_FILE
 VERIFY $? "Copying nginx configuration file"
 
 systemctl restart nginx &>>$LOG_FILE
