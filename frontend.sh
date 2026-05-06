9908299d706fc7959229e6055f96eb0375da09a5
Mahesh02-K <kakuturumahesh@gmail.com>
2026-05-07 02:35:23 +0530

    roboshop-shell

--- frontend.sh
+++ frontend.sh
@@ -57,7 +57,7 @@ unzip /tmp/frontend.zip &>>$LOG_FILE
 VERIFY $? "Unzipping frontend into temp directory"
 
 rm -rf /etc/nginx/nginx.conf &>>$LOG_FILE
-VERf /IFY $? "Remove default nginx conf"
+VERIFY $? "Remove default nginx conf"
 
 cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf &>>$LOG_FILE
 VERIFY $? "Copying nginx configuration file"
