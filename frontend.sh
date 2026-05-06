abb790e9ce60953a501e9253ac76b4769f5328af
Mahesh02-K <kakuturumahesh@gmail.com>
2026-05-07 02:40:50 +0530

    roboshop-shell

--- frontend.sh
+++ frontend.sh
@@ -46,37 +46,37 @@ systemctl enable nginx &>>$LOG_FILE
 systemctl start nginx &>>$LOG_FILE
 VERIFY $? "Starting nginx"
 
-rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
-VERIFY $? "Removing Default Content"
-
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
-VERIFY $? "Downloading Frontend Content"
-
-cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip &>>$LOG_FILE
-VERIFY $? "Unzipping frontend Content"
-
-rm -rf /etc/nginx/nginx.conf &>>$LOG_FILE
-VERIFY $? "Remove default nginx conf"
-
-cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
-VERIFY $? "Copying Nginx conf file"
-
 # rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
-# VERIFY "Removing default content"
+# VERIFY $? "Removing Default Content"
 
-# curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip 
-# VERIFY $? "Downloading frontend"
+# curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+# VERIFY $? "Downloading Frontend Content"
 
 # cd /usr/share/nginx/html 
 # unzip /tmp/frontend.zip &>>$LOG_FILE
-# VERIFY $? "Unzipping frontend into temp directory"
+# VERIFY $? "Unzipping frontend Content"
 
 # rm -rf /etc/nginx/nginx.conf &>>$LOG_FILE
 # VERIFY $? "Remove default nginx conf"
 
-# cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf &>>$LOG_FILE
-# VERIFY $? "Copying nginx configuration file"
+# cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
+# VERIFY $? "Copying Nginx conf file"
+
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
+VERIFY "Removing default content"
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip 
+VERIFY $? "Downloading frontend"
+
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip &>>$LOG_FILE
+VERIFY $? "Unzipping frontend into temp directory"
+
+rm -rf /etc/nginx/nginx.conf &>>$LOG_FILE
+VERIFY $? "Remove default nginx conf"
+
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf 
+VERIFY $? "Copying nginx configuration file"
 
 systemctl restart nginx &>>$LOG_FILE
 VERIFY $? "Restarting nginx"
