2827b49e0d0514a997022ac3f407601004525432
Mahesh02-K <kakuturumahesh@gmail.com>
2026-05-07 19:29:02 +0530

    roboshop-shell

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -13,22 +13,36 @@ LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 SCRIPT_DIR=$PWD
 
 mkdir -p $LOGS_FOLDER
-echo -e "Script started executing at : $Y $(date) $N"
+echo -e "Script started executing at : $Y $(date) $N" | tee -a $LOG_FILE
 
 if [ $USERID -eq 0 ]
 then 
-    echo -e "You are running with root access ... $G Move Forward $N"
+    echo -e "You are running with root access ... $G Move Forward $N" | tee -a $LOG_FILE
 else
-    echo -e "$R ERR :: Please run this with root access $N"
+    echo -e "$R ERR :: Please run this with root access $N" | tee -a $LOG_FILE
     exit 1
 fi
 
 VERIFY(){
     if [ $1 -eq 0 ]
     then 
-        echo -e "$2 is ... $G SUCCESS $N"
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
     else
-        echo -e "$2 is ... $R FAILURE $N"
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
     fi
 }
+
+cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>$LOG_FILE
+VERIFY $? "Copying repo file"
+
+dnf install rabbitmq-server -y &>>$LOG_FILE
+VERIFY $? "Installing rabbitmq"
+
+systemctl enable rabbitmq &>>$LOG_FILE
+systemctl start rabbitmq &>>$LOG_FILE
+VERIFY $? "Starting rabbitmq"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($END_TIME - $START_TIME))
+echo -e "Script execution completed successfully, $Y TIME_TAKEN = $TOTAL_TIME $N secs" | tee -a $LOG_FILE
